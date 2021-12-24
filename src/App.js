2d92f05bbf35206f73ff5f20aecafc7aab7a0960
EnterKaka <95547649+EnterKaka@users.noreply.github.com>
2021-12-24 19:38:11 +0100

    connect wallet

--- src/App.js
+++ src/App.js
@@ -6,6 +6,82 @@ import axios from 'axios';
 import html2canvas from 'html2canvas';
 import FormData from 'form-data';
 
+import Onboard from "bnc-onboard";
+import Web3 from "web3";
+import { ethers } from 'ethers'
+import {chainId} from "./ChainInfo.js";
+const wallets = [
+  { walletName: "metamask", preferred: true }
+];
+
+let walletProvider;
+
+const onboard = Onboard({
+
+  networkId: chainId,     // dappId: "877e8915-22d9-450e-a9b8-799bfd51798e", // [String] The API key created by step one above// [Integer] The Ethereum network ID your Dapp uses.
+  hideBranding: true,
+  walletSelect: {
+      wallets: wallets
+  },
+  subscriptions: {
+      wallet: (wallet) => {
+          walletProvider = wallet.provider;
+          console.log(`${wallet.name} is now connected`);
+      }
+  }
+});
+
+const connectWallet =  async () => {
+  const currentState = onboard.getState();
+  if(currentState["address"] != null) {
+      return {
+          address: currentState["address"],
+          status: "👆🏽 Mint your GG Now.",
+      }
+  }
+  const walletSelected = await onboard.walletSelect('MetaMask');
+  if (walletSelected !== false) {
+      const walletCheck = await onboard.walletCheck();
+      if (walletCheck === true) {
+          const currentState = onboard.getState();
+          return {
+              address: currentState["address"],
+              status: "👆🏽 Mint your GG Now.",
+          }
+      } else {
+          return {
+              address: "",
+              status: "😥 Connect your wallet account to the site.",
+          }
+      }
+  }
+
+}
+
+const disConnectWallet = () => {
+  // onboard.walletReset()
+  return {
+      address: "",
+      status: "😥 Connect your wallet account to the site.",
+  }
+}
+
+const getCurrentWalletConnected = async () => {
+  const currentState = onboard.getState();
+
+  if(currentState["address"] != null) {
+      return {
+          address: currentState["address"],
+          status: "👆🏽 Mint your GG Now.",
+      }
+  } else {
+      return {
+          address: "",
+          status: "",
+      }
+  }
+}
+
 function App() {
   // const [test, setTest] = useState(0);
   const [imgName, setImgName] = useState('');
@@ -24,7 +100,7 @@ function App() {
     );
   }
 	const upload = async ()=>{
-		if(imgurl == '') return;
+		if(imgurl === '') return;
     setLoading(true);
 		const element = await document.getElementById(imgurl);
 		const canvas = await html2canvas(element);
@@ -109,7 +185,7 @@ function App() {
   return (
     <div>
     <Loading className={loading ? '' : 'loading_disable'}/>
-		<button>mint</button>
+		<button onClick={()=>connectWallet()}>mint</button>
 		<div className="App">
 			<Monkey name={imgName} propfunc={propfunc}/>
       <input type='text' name="ImageName" value={imgName} onChange={e => changeImageName(e.target.value)} />
--- src/component/Monkey.js
+++ src/component/Monkey.js
@@ -12,39 +12,39 @@ const Monkey = ({name, propfunc, ...restProps})=>{
 
     return(
         <div {...restProps}>
-            <div className={`monkey_img${active == 'img1' ? ' active' : ''}`} >
+            <div className={`monkey_img${active === 'img1' ? ' active' : ''}`} >
                 <div 
                     id='img1' 
                     onClick={() => tempfunction('img1')}
                 >
                     <img src='img/1.jpg' alt="no img" />
-                    {active == 'img1' && (
+                    {active === 'img1' && (
                         <p className="img_name">
                             {name}
                         </p>
                     )}
                 </div>
             </div>
-            <div className={`monkey_img${active == 'img2' ? ' active' : ''}`} >
+            <div className={`monkey_img${active === 'img2' ? ' active' : ''}`} >
                 <div 
                     id='img2' 
                     onClick={() => tempfunction('img2')}
                 >
                     <img src='img/2.jpg' alt="no img" />
-                    {active == 'img2' && (
+                    {active === 'img2' && (
                         <p className="img_name">
                             {name}
                         </p>
                     )}
                 </div>
             </div>
-            <div className={`monkey_img${active == 'img3' ? ' active' : ''}`} >
+            <div className={`monkey_img${active === 'img3' ? ' active' : ''}`} >
                 <div 
                     id='img3' 
                     onClick={() => tempfunction('img3')}
                 >
                     <img src='img/3.jpg' alt="no img" />
-                    {active == 'img3' && (
+                    {active === 'img3' && (
                         <p className="img_name">
                             {name}
                         </p>
